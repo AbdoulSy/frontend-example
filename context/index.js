53f45ae860ffebb55fe679db0670f2e709caab7c
Abdoul Sy <dreescan@gmail.com>
2019-11-24 17:05:43 +0000

    feat(setup): installed prettier

--- context/index.js
+++ context/index.js
@@ -1,38 +1,36 @@
-require("@babel/polyfill")
-import React, {useState, useEffect} from "react"
-import ReactDOM from 'react-dom'
+require("@babel/polyfill");
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-export const apiCall = async (setSeconds) => {
-  const response = await fetch('https://localhost:3001/api');
+export const apiCall = async setSeconds => {
+  const response = await fetch("https://localhost:3001/api");
   const myJson = await response.json();
 
   console.log(JSON.stringify(myJson));
-  if(myJson.success) {
-    setSeconds(myJson.value)
+  if (myJson.success) {
+    setSeconds(myJson.value);
   }
-  
-}
+};
 
 export const intervalCall = (requests, setRequests) => {
   setTimeout(() => {
-    setRequests(requests + 1)
+    setRequests(requests + 1);
   }, 3000);
-}
+};
 
 export const Timer = () => {
-  const [seconds, setSeconds] = useState(0)
-  const [requests, setRequests] = useState(0)
+  const [seconds, setSeconds] = useState(0);
+  const [requests, setRequests] = useState(0);
 
   useEffect(() => {
-    apiCall(setSeconds)
-  }, [requests])
-  
+    apiCall(setSeconds);
+  }, [requests]);
+
   useEffect(() => {
-    intervalCall(requests, setRequests)
-  })
+    intervalCall(requests, setRequests);
+  });
 
   return <p>{seconds} elapsed</p>;
-}
+};
 
-
-ReactDOM.render(<Timer />, document.getElementById('root'))
+ReactDOM.render(<Timer />, document.getElementById("root"));
